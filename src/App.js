2ba494e1d40067e607ac0558941bf9f16d9ef3c6
ss35789 <ss35789@naver.com>
2024-07-03 02:31:30 +0900

    Router 설정

--- src/App.js
+++ src/App.js
@@ -1,122 +1,51 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Shop from './Shop';
 
 function App() {
     return (
-        <div>
-            {/* Navigation */}
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <div className="container px-4 px-lg-5">
-                    <a className="navbar-brand" href="#!">Start Bootstrap</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-                            <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#!">Home</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#!">About</a>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Shop</a>
-                                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a className="dropdown-item" href="#!">All Products</a></li>
-                                    <li><hr className="dropdown-divider" /></li>
-                                    <li><a className="dropdown-item" href="#!">Popular Items</a></li>
-                                    <li><a className="dropdown-item" href="#!">New Arrivals</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                        <form className="d-flex">
-                            <button className="btn btn-outline-dark" type="submit">
-                                <i className="bi-cart-fill me-1"></i>
-                                Cart
-                                <span className="badge bg-dark text-white ms-1 rounded-pill">0</span>
-                            </button>
-                        </form>
+        <Router>
+            <div>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <div className="container px-4 px-lg-5">
+                        <Link className="navbar-brand" to="/">Start Bootstrap</Link>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                                <li className="nav-item">
+                                    <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/about">About</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/shop">Shop</Link>
+                                </li>
+                            </ul>
+                            <form className="d-flex">
+                                <button className="btn btn-outline-dark" type="submit">
+                                    <i className="bi-cart-fill me-1"></i>
+                                    Cart
+                                    <span className="badge bg-dark text-white ms-1 rounded-pill">0</span>
+                                </button>
+                            </form>
+                        </div>
                     </div>
-                </div>
-            </nav>
+                </nav>
 
-            {/* Header */}
-            <header className="bg-dark py-5">
-                <div className="container px-4 px-lg-5 my-5">
-                    <div className="text-center text-white">
-                        <h1 className="display-4 fw-bolder">Shop in style</h1>
-                        <p className="lead fw-normal text-white-50 mb-0">With this shop homepage template</p>
-                    </div>
-                </div>
-            </header>
-
-            {/* Section */}
-            <section className="py-5">
-                <div className="container px-4 px-lg-5 mt-5">
-                    <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-                        {Array.from({ length: 8 }).map((_, index) => (
-                            <div className="col mb-5" key={index}>
-                                <div className="card h-100">
-                                    {/* Sale badge */}
-                                    {index % 2 === 1 && (
-                                        <div className="badge bg-dark text-white position-absolute" style={{ top: '0.5rem', right: '0.5rem' }}>Sale</div>
-                                    )}
-                                    {/* Product image */}
-                                    <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-                                    {/* Product details */}
-                                    <div className="card-body p-4">
-                                        <div className="text-center">
-                                            {/* Product name */}
-                                            <h5 className="fw-bolder">Product Name</h5>
-                                            {/* Product reviews */}
-                                            {index % 2 === 1 && (
-                                                <div className="d-flex justify-content-center small text-warning mb-2">
-                                                    <div className="bi-star-fill"></div>
-                                                    <div className="bi-star-fill"></div>
-                                                    <div className="bi-star-fill"></div>
-                                                    <div className="bi-star-fill"></div>
-                                                    <div className="bi-star-fill"></div>
-                                                </div>
-                                            )}
-                                            {/* Product price */}
-                                            {index % 2 === 1 ? (
-                                                <>
-                                                    <span className="text-muted text-decoration-line-through">$20.00</span>
-                                                    <span> $18.00</span>
-                                                </>
-                                            ) : (
-                                                <span>$40.00 - $80.00</span>
-                                            )}
-                                        </div>
-                                    </div>
-                                    {/* Product actions */}
-                                    <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                        <div className="text-center">
-                                            <a className="btn btn-outline-dark mt-auto" href="#!">Add to cart</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            </section>
-
-            {/* Footer */}
-            <footer className="py-5 bg-dark">
-                <div className="container">
-                    <p className="m-0 text-center text-white">Copyright &copy; Your Website 2023</p>
-                </div>
-            </footer>
-
-            {/* Bootstrap core JS */}
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-
-            {/* Core theme JS */}
-            <script src="js/scripts.js"></script>
-        </div>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/shop" element={<Shop />} />
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
